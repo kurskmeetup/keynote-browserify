9c7bf10c9fdbeec9d0b27773e1397f8289cc6186
Boris Okunskiy <i@savant.pro>
2015-03-29 21:55:08 +0300

    Assembled a tiny keynote

---
+++ src/app.js
@@ -0,0 +1,44 @@
+"use strict";
+
+var express = require('express')
+  , stylus = require('stylus')
+  , browserify = require('browserify-middleware')
+  , Nano = require('nanotemplates')
+  , http = require('http');
+
+var nano = new Nano({ basedir: 'views' });
+
+var app = express();
+
+app.use(stylus.middleware({
+  src: 'public',
+  compile: function(str, path) {
+    return stylus(str)
+      .set('filename', path)
+      .set('compress', true)
+      .set('include css', true)
+      .use(require('nib')())
+      .import('nib');
+  }
+}));
+
+app.use(express.static('public'));
+
+app.get('/js/index.js', browserify('src/client/index.js'));
+
+app.get('/',function(req, res, next) {
+  req.url = '/index.html';
+  next();
+});
+
+app.get('/*.html', function(req, res, next) {
+  nano.render(req.params[0] + '.html', function(err, html) {
+    /* istanbul ignore if */
+    if (err) return next(err);
+    res.send(html);
+  });
+});
+
+http.createServer(app).listen(8080, function() {
+  console.log('Type http://localhost:8080 in omnibox, bro.');
+});
---
+++ src/client/index.js
@@ -0,0 +1,5 @@
+require('../../lib/impress.js');
+impress().init();
+
+var hljs = require('highlight.js');
+hljs.initHighlightingOnLoad();
